9665c0bc84493428c1d1902a9830d3b07214d0c2
Kevin Berridge <kberridge@pointeblank.net>
2013-01-14 18:03:20 -0500

    adds deploy script

---
+++ deploy.ps1
@@ -0,0 +1,10 @@
+pushd "\projects"
+if (-not (test-path "psake")) {
+  git clone https://github.com/psake/psake.git
+}
+popd
+
+$profilePath = split-path $profile
+if (-not (test-path $profilePath)) { mkdir $profilePath |> $null }
+cp -r .\* $profilePath -exclude ".hg" -force
+. $profile
