f42fc862c0c8adadf50c803405a61052b8315bce
Kevin Berridge <kberridge@pointeblank.net>
2013-06-13 15:26:07 -0400

    sqlps: adds custom prompt, and match functions

--- Microsoft.SqlServer.Management.PowerShell.sqlps_profile.ps1
+++ Microsoft.SqlServer.Management.PowerShell.sqlps_profile.ps1
@@ -1,1 +1,14 @@
 update-formatdata C:\projects\posh-prefs\SQLProvider.Customized.Format.ps1xml
+
+function prompt {
+  Write-Host($pwd.Path) -foregroundcolor yellow
+  return "> "
+}
+
+function match-col([string]$regex) {
+  $input | ?{ $_.Name -match $regex }
+}
+
+function match-table([string]$schemaRegex, [string]$tableRegex) {
+  $input | ?{ $_.Schema -match $schemaRegex -and $_.Name -match $tableRegex }
+}
