5066d031f2c42e592eea9cc6bc07649a60530ba9
Kevin Berridge <kberridge@pointeblank.net>
2013-06-18 23:42:02 -0400

    sqlps: adds custom tab expansion fixing performance problems with sqlps

--- Microsoft.SqlServer.Management.PowerShell.sqlps_profile.ps1
+++ Microsoft.SqlServer.Management.PowerShell.sqlps_profile.ps1
@@ -20,3 +20,32 @@ function select-from([string]$table, [string[]]$cols='*', [string]$top=$nil) {
   $sql
   invoke-sqlcmd $sql
 }
+
+if ((test-path function:\TabExpansion) -and !(test-path function:\TabExpansionSqlPsBackup)) {
+  rename-item function:\TabExpansion TabExpansionSqlPsBackup
+}
+
+function TabExpansion($line, $lastWord) {
+  if (!((get-location).Provider.Name -eq "SqlServer")) {
+    TabExpansionSqlPsBackup $line $lastWord
+  }
+  else {
+    $aindex = $lastWord.LastIndexOf('\')
+    $bindex = $lastWord.LastIndexOf('/')
+    $index = (($aindex, $bindex) | measure-object -max).Maximum
+    if ($index -gt -1) { 
+      $parent = $lastWord.substring(0, $index+1) 
+      $leaf = $lastWord.substring($index+1)
+    }
+    else {
+      $parent = ""
+      $leaf = $lastWord
+    }
+
+    $matches = ls -path $parent | ?{ $_.PSChildName -match "^$leaf" }
+    if ($matches) { 
+      $matches | %{ $parent + $_.PSChildName } 
+    } 
+    else {$lastWord}
+  }
+}
